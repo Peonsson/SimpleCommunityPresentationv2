d6659bd53f8ad9ce142e70292bc33670b6ce99c9
Peonsson <xsnudelx@hotmail.com>
2015-11-28 22:23:03 +0100

    init

---
+++ SimpleCommunityPresentation/src/main/java/Beans/AddFriendBean.java
@@ -0,0 +1,21 @@
+package Beans;
+
+import BusinessLogic.UserHandler;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by Peonsson on 19/11/15.
+ */
+@ManagedBean(name="AddFriend")
+@SessionScoped
+public class AddFriendBean {
+
+    public AddFriendBean() {
+    }
+
+    public void addFriend(String userToAddAsFriend) {
+        UserHandler.addFriend(Integer.parseInt(userToAddAsFriend));
+    }
+}
---
+++ SimpleCommunityPresentation/src/main/java/Beans/UserBean.java
@@ -0,0 +1,109 @@
+package Beans;
+
+import BusinessLogic.UserHandler;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+
+/**
+ * Created by robin on 9/11/15.
+ */
+@ManagedBean(name = "User")
+@SessionScoped
+public class UserBean {
+    private int userId;
+    private String email;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String country;
+    private String city;
+
+    public UserBean() {
+        super();
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void register() {
+        boolean registerSuccess = UserHandler.registerUser(email, username, password, firstname, lastname, country, city);
+
+        if (registerSuccess) {
+            try {
+                ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+                ec.redirect(ec.getRequestContextPath() + "/index.xhtml");
+            } catch (IOException e) {
+                System.out.println("failed redirecting");
+                e.printStackTrace();
+            }
+        } else {
+            // TODO: Assign variable if registration failed. (http://stackoverflow.com/questions/15452539/redirecting-form-jsf-managed-bean-and-showing-js-alert-based-on-condition-in-man)
+        }
+    }
+}
